5d9a41ca2cdf331de7015a920ff0f40928754a4d
DeveloperOak <wsandovalrosas@gmail.com>
2024-10-22 22:59:50 -0500

    creación del proyecto e interfaz

---
+++ myTask/src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  taskList = [];
+
+
+  constructor(public navCtrl: NavController) {}
+
+  addTask(){
+    if (this.taskName.length > 0) {
+      let task = this.taskName;
+      this.taskList.push(task);
+      this.taskName = "";
+    }
+  }
+
+}
